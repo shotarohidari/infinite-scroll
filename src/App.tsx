0c9b01e6bb67a548f453a2a7b06c6ebaa49843f7
左翔太朗 <s.hidaridesu@gmail.com>
2024-06-05 16:05:57 +0900

    無限スクロールの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense, useEffect, useState } from "react"
+import "./App.css"
+import { useSuspenseQuery } from "@tanstack/react-query"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pageLength, setPageLength] = useState(5)
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.95,
+    }
+    const callback: IntersectionObserverCallback = (entries) => {
+      for (const entry of entries) {
+        if (entry.intersectionRatio >= 0.8 || entry.isIntersecting) {
+          setPageLength((pageLength) => pageLength + 5)
+        }
+      }
+    }
 
+    const observer = new IntersectionObserver(callback, options)
+
+    const lastItem = document.querySelector("#last-item")
+    if (!lastItem) {
+      console.log("last item not found.")
+      return
+    }
+    observer.observe(lastItem)
+
+    return () => {
+      observer.unobserve(lastItem)
+    }
+  })
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="grid-container">
+      {Array.from({ length: pageLength }, (_, i) => i + 1).map((id) => (
+        <div
+          key={id}
+          className="grid-item"
+          id={id === pageLength ? "last-item" : undefined}>
+          <Suspense
+            key={id}
+            fallback={
+              <div className="pokedex-skelton" key={id}>
+                読み込み中...
+              </div>
+            }>
+            <PokeDex pokeId={id} />
+          </Suspense>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const usePokeData = (id: number) => {
+  return useSuspenseQuery({
+    queryKey: [id],
+    queryFn: () =>
+      fetch(`${import.meta.env["VITE_MY_POKE_API"]}/${id}`).then(
+        (res) =>
+          res.json() as Promise<{
+            name: string
+            description: string
+            type: string[]
+            height_m: number
+            weight_kg: number
+            genus: string
+            image: string
+          }>
+      ),
+  })
+}
+
+function PokeDex(props: { pokeId: number }) {
+  const { pokeId } = props
+  const { data } = usePokeData(pokeId)
+  return (
+    <div className="pokedex">
+      <img src={data.image} />
+      <div className="pokedex-info">
+        <h2>{data.name}</h2>
+        <p>説明: {data.description}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
